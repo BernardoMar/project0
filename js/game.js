fb00224385651753faba7f442b0388e0e49ac6cb
BernardoMar <bernardomartin92@outlook.com>
2021-10-06 09:50:46 +1100

    Project0-XO

--- js/game.js
+++ js/game.js
@@ -1,19 +1,24 @@
 //Winning options
 let winingOption;
+let player = 1;
 
 
-const weAreTrying = $('#a-one');
-
-// Tries* to check whether a line has Xs.
+// Checks whether a line has Xs.
 const trial = function () {
   for (let i=0 ; i < winingOption.length ; i++) {
-    if (winingOption[i][0].text()=== 'X' && winingOption[i][0].text() === winingOption[i][1].text() && winingOption[i][0].text() === winingOption[i][2].text());
-    console.log("it's working");
+    if (winingOption[i][0].text() === 'X' &&          winingOption[i][1].text() === 'X' &&
+    winingOption[i][2].text() === 'X') {
+    alert("X Wins and Bernie is amazing")
+    $('button').text('');
+  } else if (winingOption[i][0].text() === 'O' &&          winingOption[i][1].text() === 'O' &&
+  winingOption[i][2].text() === 'O') {
+    alert("O Wins and Bernie is amazing")
+    $('button').text('');
   };
+};
 };
 
 $(document).ready (function (){
-
  winingOption = [
     [$('#a-one'), $('#a-two'),$('#a-three')],
     [$('#b-one'),$('#b-two'),$('#b-three')],
@@ -24,16 +29,21 @@ $(document).ready (function (){
     [$('#a-one'),$('#b-two'),$('#c-three')],
     [$('#a-three'),$('#b-two'),$('#c-one')]
   ];
-
   //Fun text editor plugin
   $('h1').funText(33, ['#F6F740', '#D8DC6A', '#6689A1']);
 
 
-
   // Prints X when clicked on a button
   $('button').on('click' , function(){
-    $(this).text('X');
+    if (player == 1) {
+      $(this).text('X')
+      player = 2;
+    } else if (player == 2) {
+      $(this).text('O')
+      player = 1;
+    };
 
-  trial();
+    trial();
   });
-})
+
+});
